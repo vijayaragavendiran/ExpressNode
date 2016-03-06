ade81883c62159aa0ad0c64161023f46ac47e1f2
vijayaragavendiran <vijayaragavendiran@outlook.com>
2016-03-05 18:46:20 -0800

    Included comments

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express= require('express');
 var app = express();
 var routes = require('./routes');
-app.set('view engine','ejs')
+app.set('view engine','ejs') //refers to view folder
 app.get('/',routes.index);
 app.get('/about',routes.about);
-app.get('*',function(request,response){
+app.get('*',function(request,response){ //redirects to bad routes
   response.send('Bad Route');
 })
-var server =app.listen(3000,function()
+var server =app.listen(3000,function() //listener port
 {
   console.log('Listening to port 3000');
 })
